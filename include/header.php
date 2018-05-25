de699c4b8e52a1ce6f70c5730ca656026281d786
MatiasGF <matiasngf@hotmail.com>
2018-05-25 14:41:13 -0300

    agregar php y crear seguinos

---
+++ include/header.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-36377554-9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-36377554-9');
+</script>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Argentinatón | Atomic Lab</title>
+    <meta name="description" content="Recorriendo toda la Argentina regalando prótesis impresas en 3D." />
+    <meta name="keywords" content="manos mecánicas, manos 3D, prótesis, solidaridad, impresoras 3D, argentina, solidaridad, manos mecánicas 3D, Atomic Lab, Gino Tubaro" />
+
+    <meta property="og:url" content="http://AtomicLab.org/Argentinaton">
+    <meta property="og:title" content="#Argentinatón por Atomic Lab">
+    <meta property="og:description" content="Recorriendo toda la Argentina regalando prótesis 3D.">
+    <meta property="og:image" content="http://atomiclab.org/argentinaton/public/img/link.jpg">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:type" content="website">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="http://atomiclab.org/argentinaton/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="http://atomiclab.org/argentinaton/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="http://atomiclab.org/argentinaton/favicon-16x16.png">
+<link rel="manifest" href="http://atomiclab.org/argentinaton/site.webmanifest">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+
+	<!-- Style -->
+  <link rel="stylesheet" href="public/css/styles.css">
+	<link rel="stylesheet" href="public/css/styles2.css">
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Lato|Montserrat:300,400,500,700" rel="stylesheet">
+
+	<!-- Scroll -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="public/js/slick.js" type="text/javascript" charset="utf-8"></script>
+	<script src="public/js/parallax.js" type="text/javascript" charset="utf-8"></script>
+	<script>
+		$( document ).ready(function() {
+			$('a[href*="#"]')
+				// Remove links that don't actually link to anything
+				.click(function(event) {
+				// On-page links
+				if (
+				  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+				  &&
+				  location.hostname == this.hostname
+				) {
+				  // Figure out element to scroll to
+				  var target = $(this.hash);
+				  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				  // Does a scroll target exist?
+				  if (target.length) {
+				    // Only prevent default if animation is actually gonna happen
+				    event.preventDefault();
+				    $('html, body').animate({
+				      scrollTop: target.offset().top
+				    }, 1000, function() {
+				      // Callback after animation
+				      // Must change focus!
+				      var $target = $(target);
+				      $target.focus();
+				      if ($target.is(":focus")) { // Checking if the target was focused
+				        return false;
+				      } else {
+				        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+				        $target.focus(); // Set focus again
+				      };
+				    });
+				  }
+				}
+			});
+			$(".center").slick({
+		        dots: true,
+		        arrows: false,
+		        infinite: true,
+		        centerMode: true,
+		        slidesToShow: 1,
+		        slidesToScroll: 4
+		      });
+		});
+	</script>
+	<script src='https://www.google.com/recaptcha/api.js?onload=loadCaptcha&render=explicit'></script>
+</head>
+
+<body class="">
---
+++ seguinos.php
@@ -0,0 +1,141 @@
+<?php
+	include_once('include/header.php');
+?>
+<link rel="stylesheet" href="leaflet/leaflet.css">
+<link rel="stylesheet" href="leaflet/leaflet-routing-machine.css">
+<script src="leaflet/leaflet.js"></script>
+<script src="leaflet/leaflet-routing-machine.js"></script>
+
+<style media="screen">
+	#mainMap{
+		width: 100vw;
+		height: 30vw;
+	}
+	.listaEventos{
+		position: absolute;
+		height: 100%;
+		width: 300px;
+	}
+</style>
+<div class="containerMap">
+	<div class="listaEventos">
+
+	</div>
+	<div class="" id="mainMap"></div>
+</div>
+
+<script type="text/javascript">
+	let eventos = {
+		1234565: {
+			type: 'entrega',
+			date: 1525449989,
+			latLan: [51.5, -0.09],
+			data: {
+				img: 'nene001.jpg',
+				desc: 'Hoy entregamos una mano felizmente en londres. Gino no deja de roncar por las noches.'
+			}
+		},
+		2146464: {
+			type: 'entrega',
+			date: 1525489989,
+			latLan: [51.53, -0.091],
+		},
+		4456464: {
+			type: 'entrega',
+			date: 1525499989,
+			latLan: [51.525, -0.2],
+		},
+	}
+
+	let tokenMap = 'pk.eyJ1IjoibWF0aWFzZ2YiLCJhIjoiY2pnc205Mmo2MDJsdzMzbjI2dHVsZnNhYSJ9.GHBkK7xLQz1C6DYDmQqXTw';
+	let mapAtom = L.map('mainMap').setView([51.5, -0.09],12);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+tokenMap, {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox.streets',
+		accessToken: tokenMap
+	}).addTo(mapAtom);
+
+	//icons
+	let iconoAtomico = L.icon({
+    iconUrl: 'public/img/icons/geoAtomic.svg',
+		shadowUrl: 'public/img/icons/sombrageoAtomic.png',
+    iconSize:     [64, 64],
+		shadowSize:   [64, 64],
+		iconAnchor:   [32, 64],
+    shadowAnchor: [32, 64],
+	});
+
+	let ruta = L.Routing.control({
+  	waypoints: [],
+  	routeWhileDragging: false,
+		draggableWaypoints: false,
+		addWaypoints: false,
+		createMarker: false,
+		lineOptions: {
+			styles: [
+				{color: '#f27329', opacity: 1, weight: 4},
+			]
+		},
+  	// geocoder: L.Control.Geocoder.nominatim()
+	});
+	let nodosRuta = [];
+
+	//eventos
+	$.each(eventos, function(k,o){
+		eventos[k].obj = {};
+		eventos[k].obj = L.marker(o.latLan).addTo(mapAtom);
+		eventos[k].obj.id = k;
+		eventos[k].obj.on('click', markerclick);
+		if(o.type=='entrega'){
+			eventos[k].obj.setIcon(iconoAtomico);
+		}
+		nodosRuta.push(L.latLng(o.latLan[0], o.latLan[1]));
+	});
+	ruta.setWaypoints(nodosRuta);
+	ruta.addTo(mapAtom);
+	ruta.route();
+	console.log(ruta);
+
+	function markerclick(e){
+		createPopup(eventos[this.id]);
+		console.log(this.id);
+	}
+
+	function createPopup(obj){
+		let html = '<div class="mainPop">';
+		let hasHeadImg = '';
+		let headerUrl = false;
+		if('data' in obj){
+			if('img' in obj.data && obj.data != ''){
+				hasHeadImg = ' hasHeaderImg';
+				headerUrl = 'public/img/gallery/'+obj.data.img;
+			}
+		}
+		html+='<div class="popHeader'+hasHeadImg+'">';
+		if(obj.type=='entrega'){
+			html+='<h2>Entrega</h2>';
+		}
+		if(headerUrl){
+			html+='<div class="fondoHead" style="background:url(\''+headerUrl+'\')"></div>';
+		}
+		html+='</div>';//end header
+			html+='<div class="popContent">';
+			if('data' in obj && 'desc' in obj.data && obj.data.desc!=''){
+				html+='<p class="descripcion">'+obj.data.desc+'</p>';
+			}
+			html+='</div>';//end content
+		html+='</div>';//end main
+		$('body').append(html);
+		console.log(html);
+	}
+	let pooop = L.popup()
+    .setLatLng(obj.latLan)
+    .setContent(html)
+    .openOn(mapAtom);
+</script>
+
+
+<?php
+	include_once('include/footer.php');
+?>
