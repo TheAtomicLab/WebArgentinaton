70e4ac017fd1c162b784d7e4d0de249a1a89abef
MatiasGF <matiasngf@hotmail.com>
2018-08-08 22:46:09 -0300

    Mapa funcionando bien

---
+++ public/js/jsonentregas.js
@@ -0,0 +1,61 @@
+var eventos = {
+	1234565: {
+		type: 'entrega',
+		cantidadEntragadas: 1, //v_label
+		visited: true,
+		date: 1525449989, //v_dia, v_mes
+		latLan: [51.5, -0.09], //v_location
+		region: {
+			pais: 'Argentina',
+			provincia: 'Córdoba',
+			ciudad: 'La Cumbre',
+		},
+		data: {
+			img: 'nene001.jpg', //v_imagen
+			desc: '' //v_texto
+		},
+	},
+	2146464: {
+		type: 'entrega',
+		cantidadEntragadas: 1,
+		visited: true,
+		date: 1525489989,
+		latLan: [51.53, -0.091],
+		region: {
+			pais: 'Argentina',
+			provincia: 'Córdoba',
+			ciudad: 'El Durazno',
+		},
+	},
+	4456464: {
+		type: 'entrega',
+		cantidadEntragadas: 1,
+		visited: true,
+		date: 1525499989,
+		latLan: [51.525, -0.2],
+		region: {
+			pais: 'Argentina',
+			provincia: 'Córdoba',
+			ciudad: 'Pilar',
+		},
+	},
+	4456465: {
+		type: 'entrega',
+		cantidadEntragadas: 1,
+		visited: false,
+		date: 1526774400,
+		latLan: [51.525, -0.53],
+		region: {
+			pais: 'Argentina',
+			provincia: 'Formosa',
+			ciudad: 'Las Varillas',
+		},
+		data: {
+			img: 'nene001.jpg', //v_imagen
+			desc: '' //v_texto
+		},
+	},
+};
+
+
+eventos = {"0":{"type":"point","cantidadEntragadas":1,"date":"1532055600","latLan":[-34.63386937024641,-58.41066111740434],"region":{"provincia":"Ciudad Autónoma de Buenos Aires","ciudad":"Parque Patricios"}},"1":{"type":"entrega","cantidadEntragadas":1,"date":"1532142000","latLan":[-31.64683999999994,-60.37596999999994],"region":{"provincia":"Entre Ríos","ciudad":"Villa Urquiza"}},"2":{"type":"entrega","cantidadEntragadas":1,"date":"1532228400","latLan":[-31.454878134482097,-60.925472245538984],"region":{"provincia":"Santa Fe","ciudad":"Esperanza"}},"3":{"type":"entrega","cantidadEntragadas":1,"date":"1532228400","latLan":[-31.445079069238698,-61.25892347364287],"region":{"provincia":"Santa Fe","ciudad":"Pilar"}},"4":{"type":"entrega","cantidadEntragadas":1,"date":"1532314800","latLan":[-31.435378701315614,-62.08293789710456],"region":{"provincia":"Córdoba","ciudad":"San Francisco"}},"5":{"type":"entrega","cantidadEntragadas":1,"date":"1532314800","latLan":[-31.438728814213984,-62.088818149655886],"region":{"provincia":"Córdoba","ciudad":"San Francisco"}},"6":{"type":"entrega","cantidadEntragadas":1,"date":"1532401200","latLan":[-31.402371187488278,-64.47949785936248],"region":{"provincia":"Córdoba","ciudad":"Villa Carlos Paz"}},"7":{"type":"entrega","cantidadEntragadas":1,"date":"1532487600","latLan":[-31.482088333541025,-64.21203621429525],"region":{"provincia":"Córdoba","ciudad":"Córdoba"}},"8":{"type":"entrega","cantidadEntragadas":1,"date":"1532487600","latLan":[-31.473510911101027,-64.21830198810497],"region":{"provincia":"Córdoba","ciudad":"Córdoba"}},"9":{"type":"entrega","cantidadEntragadas":1,"date":"1532487600","latLan":[-32.76043999999996,-63.77971999999994],"region":{"provincia":"Córdoba","ciudad":"General Deheza"}},"10":{"type":"entrega","cantidadEntragadas":1,"date":"1532574000","latLan":[-30.7301171117915,-64.79271630859346],"region":{"provincia":"Córdoba","ciudad":"Cruz del Eje"}},"11":{"type":"entrega","cantidadEntragadas":1,"date":"1532660400","latLan":[-28.314169999999933,-67.05006999999995],"region":{"provincia":"La Rioja","ciudad":"San Blas de los Sauces"}},"12":{"type":"entrega","cantidadEntragadas":1,"date":"1532746800","latLan":[-29.413359999999955,-66.85589999999996],"region":{"provincia":"La Rioja","ciudad":"La Rioja"}},"13":{"type":"entrega","cantidadEntragadas":1,"date":"1532833200","latLan":[-27.49422884728525,-64.86918965984401],"region":{"provincia":"Santiago del Estero","ciudad":"Termas de Río Hondo"}},"14":{"type":"entrega","cantidadEntragadas":1,"date":"1532833200","latLan":[-27.26677217984083,-65.35698976970173],"region":{"provincia":"Tucumán","ciudad":"Simoca"}},"15":{"type":"entrega","cantidadEntragadas":1,"date":"1532919600","latLan":[-26.84775729011825,-65.20236111468702],"region":{"provincia":"Tucumán","ciudad":"San Miguel de Tucumán"}},"16":{"type":"entrega","cantidadEntragadas":1,"date":"1532919600","latLan":[-26.827729999999974,-65.21044999999998],"region":{"provincia":"Tucumán","ciudad":"San Miguel de Tucumán"}},"17":{"type":"entrega","cantidadEntragadas":1,"date":"1532919600","latLan":[-26.798591518682006,-65.18400170466381],"region":{"provincia":"Tucumán","ciudad":"San Miguel de Tucumán"}},"18":{"type":"entrega","cantidadEntragadas":1,"date":"1533006000","latLan":[-25.795551092128914,-64.97096385400201],"region":{"provincia":"Salta","ciudad":"Rosario de la Frontera"}},"19":{"type":"entrega","cantidadEntragadas":1,"date":"1533092400","latLan":[-24.77068475978985,-65.3977194130864],"region":{"provincia":"Salta","ciudad":"Salta"}},"20":{"type":"entrega","cantidadEntragadas":1,"date":"1533092400","latLan":[-24.800126022202896,-65.42047591288839],"region":{"provincia":"Salta","ciudad":"Salta"}},"21":{"type":"entrega","cantidadEntragadas":1,"date":"1533178800","latLan":[-24.19638928776232,-65.3016651357846],"region":{"provincia":"Jujuy","ciudad":"San Salvador de Jujuy"}},"22":{"type":"entrega","cantidadEntragadas":1,"date":"1533178800","latLan":[-24.24127999999996,-65.25164999999998],"region":{"provincia":"Jujuy","ciudad":"Alto Comedero"}},"23":{"type":"entrega","cantidadEntragadas":1,"date":"1533178800","latLan":[-24.376499999999965,-65.11600999999996],"region":{"provincia":"Jujuy","ciudad":"Perico"}},"24":{"type":"entrega","cantidadEntragadas":1,"date":"1533178800","latLan":[-24.379772581573686,-65.11450528626597],"region":{"provincia":"Jujuy","ciudad":"Perico"}},"25":{"type":"entrega","cantidadEntragadas":1,"date":"1533351600","latLan":[-22.515859999999975,-63.79293999999999],"region":{"provincia":"Salta","ciudad":"Tartagal"}},"26":{"type":"point","cantidadEntragadas":1,"date":"1533351600","latLan":[-24.708019999999976,-60.59467999999998],"region":{"provincia":"Formosa","ciudad":"Las Lomitas"}},"27":{"type":"entrega","cantidadEntragadas":1,"date":"1533438000","latLan":[-26.167069999999967,-58.20113999999995],"region":{"provincia":"Formosa","ciudad":"Formosa"}},"28":{"type":"entrega","cantidadEntragadas":1,"date":"1533524400","latLan":[-26.17908004987247,-58.17547239484354],"region":{"provincia":"Formosa","ciudad":"Formosa"}},"29":{"type":"entrega","cantidadEntragadas":1,"date":"1533610800","latLan":[-27.48699596947223,-58.93196296540186],"region":{"provincia":"Chaco","ciudad":"Barranqueras"}},"30":{"type":"entrega","cantidadEntragadas":1,"date":"1533697200","latLan":[-27.463533686016977,-58.83040564567621],"region":{"provincia":"Corrientes","ciudad":"Corrientes"}},"31":{"type":"entrega","cantidadEntragadas":1,"date":"1533783600","latLan":[-27.220948232363785,-61.188043825178255],"region":{"provincia":"Chaco","ciudad":"Charata"}},"32":{"type":"entrega","cantidadEntragadas":1,"date":"1533783600","latLan":[-27.218309999999974,-61.18774999999994],"region":{"provincia":"Chaco","ciudad":"Charata"}},"33":{"type":"entrega","cantidadEntragadas":1,"date":"1533783600","latLan":[-26.539520444501957,-59.34680999999999],"region":{"provincia":"Chaco","ciudad":"General José de San Martín"}},"34":{"type":"entrega","cantidadEntragadas":1,"date":"1533956400","latLan":[-27.47999999999996,-55.829069999999945],"region":{"provincia":"Misiones","ciudad":"Garupá"}},"35":{"type":"entrega","cantidadEntragadas":1,"date":"1533956400","latLan":[-27.401570999999997,-55.898540999999994],"region":{"provincia":"Misiones","ciudad":"Posadas"}},"36":{"type":"entrega","cantidadEntragadas":1,"date":"1533956400","latLan":[-27.39788501825659,-55.92084814175116],"region":{"provincia":"Misiones","ciudad":"Posadas"}},"37":{"type":"entrega","cantidadEntragadas":1,"date":"1534129200","latLan":[-26.616669999999942,-54.133329999999944],"region":{"provincia":"Misiones","ciudad":"San Vicente"}},"38":{"type":"entrega","cantidadEntragadas":1,"date":"1534129200","latLan":[-27.48082999999997,-54.96319999999997],"region":{"provincia":"Misiones","ciudad":"Villa Bonita"}},"39":{"type":"entrega","cantidadEntragadas":1,"date":"1534129200","latLan":[-27.912170544735492,-55.75734985999021],"region":{"provincia":"Misiones","ciudad":"Apóstoles"}},"40":{"type":"entrega","cantidadEntragadas":1,"date":"1534215600","latLan":[-29.71563999999995,-57.09520999999995],"region":{"provincia":"Corrientes","ciudad":"Paso de los Libres"}},"41":{"type":"entrega","cantidadEntragadas":1,"date":"1534215600","latLan":[-31.359537820585047,-58.03456371432862],"region":{"provincia":"Entre Ríos","ciudad":"Concordia"}},"42":{"type":"entrega","cantidadEntragadas":1,"date":"1534302000","latLan":[-32.48411154149454,-58.237689240590385],"region":{"provincia":"Entre Ríos","ciudad":"Concepción del Uruguay"}},"43":{"type":"entrega","cantidadEntragadas":1,"date":"1534302000","latLan":[-32.48655999999994,-58.24759999999998],"region":{"provincia":"Entre Ríos","ciudad":"Concepción del Uruguay"}},"44":{"type":"entrega","cantidadEntragadas":1,"date":"1534302000","latLan":[-32.688245047147525,-58.87623482358641],"region":{"provincia":"Entre Ríos","ciudad":"Urdinarrain"}},"45":{"type":"entrega","cantidadEntragadas":1,"date":"1534302000","latLan":[-33.00470036550392,-58.502787523796904],"region":{"provincia":"Entre Ríos","ciudad":"Gualeguaychú"}}};
